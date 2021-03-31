c96562977dc870498aef89245761af1c74e6493a
Dzmitry Duboyski <dzmitry.duboyski@gmail.com>
2021-03-31 11:30:32 +0300

    fix: fixed JS code

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
+var addButton=document.getElementById("btn-add-task");//first button
 var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
@@ -33,18 +33,21 @@ var createNewTaskElement=function(taskString){
   var deleteButtonImg=document.createElement("img");//delete button image
 
   label.innerText=taskString;
-  label.className='task';
+  label.className='task__label';
 
   //Each elements, needs appending
+  listItem.className="task";
   checkBox.type="checkbox";
+  checkBox.className="task__input-checkbox";
   editInput.type="text";
-  editInput.className="task";
+  editInput.className="task__input-text";
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="edit";
+  editButton.className="task__btn-edit";
 
-  deleteButton.className="delete";
+  deleteButton.className="task__btn-delete";
   deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.className='task__btn-img';
   deleteButton.appendChild(deleteButtonImg);
 
   //and appending.
@@ -80,9 +83,9 @@ var editTask=function(){
 
   var listItem=this.parentNode;
 
-  var editInput=listItem.querySelector('input[type=text]');
+  var editInput=listItem.querySelector('.task__input-text');
   var label=listItem.querySelector("label");
-  var editBtn=listItem.querySelector(".edit");
+  var editBtn=listItem.querySelector(".task__btn-edit");
   var containsClass=listItem.classList.contains("editMode");
   //If class of the parent is .editmode
   if(containsClass){
@@ -149,8 +152,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
   console.log("bind list item events");
   //select ListItems children
   var checkBox=taskListItem.querySelector("input[type=checkbox]");
-  var editButton=taskListItem.querySelector("button.edit");
-  var deleteButton=taskListItem.querySelector("button.delete");
+  var editButton=taskListItem.querySelector("button.task__btn-edit");
+  var deleteButton=taskListItem.querySelector("button.task__btn-delete");
 
   //Bind editTask to edit button.
   editButton.onclick=editTask;
